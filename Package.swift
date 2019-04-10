73283c34fe89138f376b93cab707674af036fc2c
Enzo Chiaro <enzo@finalcad.com>
2019-04-10 13:27:13 +0200

    Add Swift 5 support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,17 @@
+// swift-tools-version:5.0
 import PackageDescription
 
-
 let package = Package(
-  name: "querykit",
-  dependencies: [
-    .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 9),
-    .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
-  ]
+    name: "querykit",
+    dependencies: [
+        .package(url: "https://github.com/kylef/Stencil.git",from: "0.13.0"),
+        .package(url: "https://github.com/kylef/Commander.git",  from: "0.8.0")
+    ],
+    targets: [
+        .target(
+            name: "QueryKit",
+            dependencies: ["Stencil", "Commander"],
+            path: "Sources"
+        )
+    ]
 )
