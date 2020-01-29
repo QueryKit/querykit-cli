e5dadb6ce1dacae2500b18aa0ecd3ab4f9c188c4
Kyle Fuller <kyle@fuller.li>
2020-01-29 02:31:46 +0000

    refactor: switch to swift 5

--- Package.swift
+++ Package.swift
@@ -1,10 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 
 let package = Package(
   name: "querykit",
+  products: [
+    .executable(name: "querykit", targets: ["querykit-cli"]),
+  ],
   dependencies: [
-    .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 9),
-    .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
+    .package(url: "https://github.com/kylef/Stencil.git", from: "0.9.0"),
+    .package(url: "https://github.com/kylef/Commander.git", from: "0.6.0"),
+  ],
+  targets: [
+    .target(name: "querykit-cli", dependencies: ["Stencil", "Commander"]),
   ]
 )
